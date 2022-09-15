f15a4edba50b3538aa674a358ee36c0f00a69ee5
Roman Chesanov <romanchesanov@outlook.com>
2022-09-15 17:07:20 +0300

    Second Commit

--- src/test/java/guru/qa/FirstClass.java
+++ src/test/java/guru/qa/FirstClass.java
@@ -1,4 +1,12 @@
 package guru.qa;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FirstClass {
+
+    @Test
+    void firstSimpleTest () {
+        Assertions.assertTrue(3 > 2);
+    }
 }
